c956ffe519715b374b5e76393948c06698de6a62
reecelikesramen <3149587+reecelikesramen@users.noreply.github.com>
2025-01-15 21:57:45 -0600

    Set up router, stub receipts/process endpoint, read PORT from env

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+const DefaultPort = "8080"
+
 type item struct {
 	ShortDescription string `json:"shortDescription"`
 	Price            string `json:"price"`
@@ -11,4 +20,21 @@ type receipt struct {
 	PurchaseTime string `json:"purchaseTime"`
 	Items        []item `json:"items"`
 	Total        string `json:"total"`
+}
+
+func main() {
+	router := gin.Default()
+	router.POST("/receipts/process", postReceipt)
+
+	// port from env or default
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = DefaultPort
+	}
+
+	router.Run(":" + port)
+}
+
+func postReceipt(c *gin.Context) {
+	c.IndentedJSON(http.StatusCreated, "hello")
 }
